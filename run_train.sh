71dfb1e9c02eef9054f4f45d2c66e1530b95b729
Ubuntu <ubuntu@ip-172-31-48-111.us-west-2.compute.internal>
2024-04-05 18:57:09 +0000

    add slurm scripts

---
+++ run_train.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+source ./setup.sh
+source ./train_setup.sh
+
+mkdir c4_test_dump -p
+OUTPUT_DIR=./c4_test_dump
+DATA_DIR=gs://axlearn-public/tensorflow_datasets
+
+python3 -m axlearn.common.launch_trainer_main \
+        --module=text.gpt.c4_trainer --config=fuji-test \
+        --trainer_dir=$OUTPUT_DIR --data_dir=$DATA_DIR --jax_backend=neuron
