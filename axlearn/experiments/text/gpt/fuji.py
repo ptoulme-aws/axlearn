3dd5f943cedb47798f3166cfa153cb99bb094f91
Apoorv Gupta <apoorvgu@amazon.com>
2024-04-11 18:35:29 +0000

    moved scan based GA to use config and cleaner

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -52,7 +52,7 @@ def get_trainer_kwargs(model_size: str, *, vocab_size: int) -> Dict[str, Any]:
             ),
             input_partition_type=DataPartitionType.DATA,
             max_sequence_length=4096,
-            train_batch_size=DP_DEGREE,
+            train_batch_size=DP_DEGREE * GRADIENT_ACCUM_STEPS,
             max_step=20000,
             gradient_accumulation_microbatches=GRADIENT_ACCUM_STEPS,
             mesh_shape=mesh_shape_from_axes(data=DP_DEGREE, model=TP_DEGREE),
