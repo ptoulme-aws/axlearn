8b8d6774e96d38082680b45c9b1b5d3dafba93bd
Indu <thangakr@amazon.com>
2024-08-08 11:03:25 -0700

    Keep flags in a separate file

--- flags.sh
+++ flags.sh
@@ -1,46 +1,34 @@
-#!/usr/bin/env bash
-
-cd /axlearn
-
-if [ -z "${TEST_ARTIFACTS_PATH}" ]; then
-  ARTIFACTS_PATH="/artifacts"
-  TIMESTAMP=$(date +"%y%m%d%H%M%S")
-  TEST_ARTIFACTS_PATH="${ARTIFACTS_PATH}/${TIMESTAMP}"
-fi
-mkdir -p "$TEST_ARTIFACTS_PATH"
-
 NEURON_DUMP_PATH=${TEST_ARTIFACTS_PATH}/neuron_dump
 HLO_DUMP_PATH=${TEST_ARTIFACTS_PATH}/hlo_dump
-export XLA_FLAGS="--xla_dump_hlo_as_text --xla_dump_hlo_as_proto --xla_dump_to=${HLO_DUMP_PATH} --xla_dump_hlo_pass_re='.*'"
+export XLA_FLAGS="--xla_dump_hlo_as_text --xla_disable_hlo_passes=aws_neuron_flip_all_gather_dot --xla_dump_hlo_as_proto --xla_dump_to=${HLO_DUMP_PATH} --xla_dump_hlo_pass_re='.*'"
 
 vnc_size=2
 
 # Neuron compiler flags
 export NEURON_CC_FLAGS="--framework=XLA"
-export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --target=trn2"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --target=trn2 --distribution-strategy=llm-training --internal-compiler-debug-mode=penguin"
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-num-neuroncores-per-sengine=${vnc_size} --internal-hlo2tensorizer-options='--verify-hlo --fuse-dot-logistic=false'"
 export NEURON_RT_VIRTUAL_CORE_SIZE=${vnc_size}
 export NEURON_RT_RESET_CORES=1
 export NEURON_RT_LOG_LEVEL="WARNING"
-
+#--internal-compiler-debug-mode=all
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --target=trn2"
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --model-type transformer"
-# export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --distribution-strategy=llm-training"
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --no-internal-hlo-remat"
+# export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --distribution-strategy=llm-training"
 # export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-hlo2tensorizer-options='--verify-hlo --num-concat-graphs=8'" # Set indside fuji.py with gradient_accumulation size
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --enable-mixed-precision-accumulation"
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} -O1"
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --dump=${NEURON_DUMP_PATH}"
-# export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --disable-internal-dge"
-export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-compiler-debug-mode=penguin"
+#export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --disable-internal-dge"
 
 # Neuron PJRT flags
 export NEURON_WHILE_LOOP_UNROLL=1
 export NEURON_RUN_TRIVIAL_COMPUTATION_ON_CPU=1
-# export NEURON_TRANSFORMER_SHARDING=1
+#export NEURON_TRANSFORMER_SHARDING=1
 
 # Neuron runtime flags
-# export NEURON_RT_ASYNC_EXEC_MAX_INFLIGHT_REQUESTS=1
+export NEURON_RT_ASYNC_EXEC_MAX_INFLIGHT_REQUESTS=1
 
 # Neuron env vars for distributed training based on SLURM
 # nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
@@ -58,12 +46,3 @@ export NEURON_RUN_TRIVIAL_COMPUTATION_ON_CPU=1
 # export FI_EFA_USE_DEVICE_RDMA="1"
 # export FI_PROVIDER="efa"
 # export FI_EFA_FORK_SAFE=1
-
-OUTPUT_DIR="${TEST_ARTIFACTS_PATH}/axlearn_out"
-mkdir -p ${OUTPUT_DIR}
-DATA_DIR="gs://axlearn-public/tensorflow_datasets"
-# Run the training script
-python -m axlearn.common.launch_trainer_main \
-  --module=text.gpt.c4_trainer --config=fuji-7B-v1 \
-  --trainer_dir=$OUTPUT_DIR --data_dir=$DATA_DIR \
-  --jax_backend=neuron --mesh_selector=trn2
